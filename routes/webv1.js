74d10e53c604d81d2b4fabc0e26e80edf971ff1d
Jagannath Maharana <ashvinmaharana007@gmail.com>
2021-02-16 14:28:28 +0530

    dataset

--- routes/webv1.js
+++ routes/webv1.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var config = require("../config");
-var indexCtrl = require("../controllers/index_controller");
+var webv1Ctrl = require("../controllers/webv1_controller");
 
 
 //Web Apis
-router.post('/login',indexCtrl.loginRequest);
-router.post('/createAdmin', indexCtrl.createAdmin);
+router.post('/login',webv1Ctrl.loginRequest);
+router.post('/createAdmin', webv1Ctrl.createAdmin);
 module.exports = router;
