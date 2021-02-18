96670b8bf90955903334b9fd285fdb41824bacdb
ashvinm007 <ashvinmaharana007@gmail.com>
2021-02-18 09:05:05 +0530

    Orders, Area, Package Contents, Message Templete

--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,24 @@ router.get("/dashboard", function (req, res, next) {
   res.render("pages/index");
 });
 
+router.get("/servingareas", function (req, res, next) {
+  res.render("pages/serving_areas");
+});
+
+router.get("/packagecontents", function (req, res, next) {
+  res.render("pages/package_contents");
+});
+
+router.get("/messagetempletes", function (req, res, next) {
+  res.render("pages/message_templetes");
+});
+
 router.get("/orders", (req, res, next) => {
-  res.render('pages/orders');
+  res.render("pages/orders");
 });
 
 router.get("/drivers", (req, res, next) => {
-  res.render('pages/drivers');
+  res.render("pages/drivers");
 });
 
 module.exports = router;
